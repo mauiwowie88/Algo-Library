037efbc89e2acfce72e0535bf7d3c55a112301e2
mauiwowie88 <maugarza96@gmail.com>
2023-05-14 22:06:29 -0700

    Work in progress.

---
+++ Arrays/contains-dupilcate.js
@@ -0,0 +1,10 @@
+/*
+Given an integer array nums, return true if any value appears at least twice in the array,
+and return false if every element is distinct.
+
+Input: nums = [1,2,3,1]
+Output: true
+
+Input: nums = [1,2,3,4]
+Output: false
+*/
