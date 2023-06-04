b9c06724d21741ae4ca445d1d392fa62a632328d
mauiwowie88 <maugarza96@gmail.com>
2023-06-03 21:40:03 -0700

    Thank you algo god.

--- Arrays/best-time-to-buy-stock2.js
+++ Arrays/best-time-to-buy-stock2.js
@@ -30,8 +30,8 @@ const array = [1,7,8,3,6,4];
 // 1st attempt: time O(n) | space O(1)
 const mostProfit = (arr) => {
     let profit = 0;
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] < arr[i + 1]) profit += arr[i + 1] - arr[i]
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i-1] < arr[i]) profit += arr[i] - arr[i-1];
     }
     return profit;
 }
--- Arrays/boats-to-save-people.js
+++ Arrays/boats-to-save-people.js
@@ -1,4 +1,7 @@
 /* 
+You are given an array people where people[i] is the weight of the ith person, and an infinite number of boats where each boat can carry a maximum weight of limit. Each boat carries at most two people at the same time, provided the sum of the weight of those people is at most limit.
+
+Return the minimum number of boats to carry every given person.
 
  Input: people = [1,2], limit = 3
 Output: 1
@@ -12,17 +15,6 @@ Input: people = [3,5,3,4], limit = 5
 Output: 4
 Explanation: 4 boats (3), (3), (4), (5)
 
-Input: people = [3,2,2,1,3,2,2,1,1], limit = 3
-Boats: 6
-Explanation: (3)(3)(2,1)(2,1)(2,1)(2)
-
-Input: people = [3,5,3,4], limit = 5
-Boats: 4
-Explanation: (3)(3)(4)(5)
-
-[1,3,2,2,8,6,7] (10) = [9,7,8,8,2,4,3]
-1,2,2,3,6,7,8] [6] = []
-
 */
 
 const array1 = [3,2,2,1,3,2,2,1,1];
@@ -30,17 +22,19 @@ const array2 = [3,5,3,4];
 
 // 1st attempt: time O(n log n) | space O(1)
 const boats = (array, limit) => {
-    array.sort((a, b) => a - b);
+    // Made coopy of array sorted
+    const copy = array.sort((a, b) => a - b);
+    // This is our front pointer that traverses forward on the array
     let front = 0;
-    let back = array.length - 1;
+    // This is our back pointer that traverses backward on the array
+    let back = copy.length - 1;
+    // 
     let count = 0;
     while (front <= back) {
-      const remaining = limit - array[r];
+      const remaining = limit - copy[back];
       back--;
       count++;
-      if (remaining >= array[front] && front <= back) {
-        front++;
-      }
+      if (remaining >= copy[front] && front <= back) front++;
     }
     return count;
 }
@@ -54,5 +48,24 @@ console.log(boats(array1, 10))
 
 /* Algo God Approach
     This is how Zack the Free Man didit.
-
+    Approach: Sort and greedy/two pointer approach approach
+    O(nlog(n)) time | O(1) space where n is the length of people array
 */
+
+
+
+const numRescueBoats = (people, limit) => {
+  people.sort((a, b) => a - b);
+  let l = 0;
+  let r = people.length - 1;
+  let count = 0;
+  while (l <= r) {
+    const remaining = limit - people[r];
+    r--;
+    count++;
+    if (remaining >= people[l] && l <= r) {
+      l++;
+    }
+  }
+  return count;
+};
