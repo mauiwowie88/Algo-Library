7eb90a6812e1c8662065a0fcc2f70e8d3aa5f205
mauiwowie88 <maugarza96@gmail.com>
2023-06-11 23:36:35 -0700

    Jun 11

--- Arrays/implement-queue.js
+++ Arrays/implement-queue.js
@@ -11,37 +11,42 @@ Notes:
 
 You must use only standard operations of a stack, which means only push to top, peek/pop from top, size, and is empty operations are valid.
 Depending on your language, the stack may not be supported natively. You may simulate a stack using a list or deque (double-ended queue) as long as you use only a stack's standard operations.
-
+Can you implememnt the queue such that each operation is O(1)
 */
 
 
 
 
+// 2nd attempt:
 class Queue {
   constructor() {
-    this.stack1 = [];
-    this.stack2 = []; 
+    this.pushStack = [];
+    this.popStack = []; 
+
   }
   push(num) {
-    this.stack1.push(num)
-    return this.stack1;
+    this.pushStack.push(num);
+    return this.pushStack;
   }
   pop() {
-    if (!this.stack1[0]) return 'Stack is empty';
-    const first = this.stack1.shift();
-    this.stack2.push(first);
-    return first;
+    if (!this.popStack.length) {
+      while(this.pushStack.length) {
+        const curr = this.pushStack.pop();
+        this.popStack.push(curr);
+      }
+    }
+    return this.popStack.pop()
   }
 
   peek() {
-    return this.stack1[0];
+    if (this.popStack.length) return this.popStack[this.popStack.length-1];
+    return pushStack[0];
   }
 
   empty() {
-      if (!this.stack1.length && !this.stack2.length) return true;
+    return !this.pushStack.length && !this.popStack.length;
   }
 }
-
 
 
 
