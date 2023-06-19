029dd3734fb8571e5d1b6aae237d0d3ec7a05ec3
mauiwowie88 <maugarza96@gmail.com>
2023-06-19 14:55:59 -0700

    Jun 19.

--- Arrays/kth-missing-positive.js
+++ Arrays/kth-missing-positive.js
@@ -13,6 +13,32 @@ Explanation: The missing positive integers are [5,6,7,...]. The 2nd missing posi
 
 */
 
+// 1st attempt: |
+const missing = (arr, num) => {
+    // This is to store all the missing nums
+    let missing = [];
+    // To keep track of kth positive integer
+    let counter = num;
+    // Reverse arr to usse pop intstead of shift
+    const reversed = [...arr].reverse();
+    // For loop until you find all missing nums
+    for (let i = 1; i <= Infinity; i++) {
+        // This is the curr last el in reversed arr
+        const last = reversed[reversed.length-1];
+        // If counter ever 0; return the last missing num
+        if (!counter) return missing.pop();
+        // If curr i === last; remove the last el 
+        if (i === last) reversed.pop();
+        // Else decrease counter & push num to missing
+        else {
+            counter--
+            missing.push(i)
+        }
+    }
+    // If we make it to this line, we're in an alternate reality
+    return 'Really?';
+}
+
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
--- Arrays/max-consecutive-ones.js
+++ Arrays/max-consecutive-ones.js
@@ -10,6 +10,54 @@ Output: 2
 
 */
 
+// 1st attempt: |
+const binary = (arr) => {
+  // This keeps track of 1s in queue
+  let queue = [];
+  // This is to keep all arrays of consecutive ones
+  const output = [];
+  // Loop over arr
+  for (const num of arr) {
+      // If num === 1 push num onto queue
+      if (num === 1) queue.push(num)
+      // If num === 0 push queue into output and clear queue
+      else {
+          output.push(queue);
+          queue = [];
+      }
+  }
+  // Once done push queue into output
+  output.push(queue);
+  // Then return the output sorted from shortest to longest and remove the last el length
+  return output.sort((a,b) => a.length-b.length).pop().length
+}
+
+
+// 2nd attempt: |
+const longest = (arr) => {
+  // This is to keep track of curr consecutives 1s
+  let curr = 0;
+  // This is to keep track of the overall highest
+  let highest = 0;
+  // Loop over arr
+  for (const num of arr) {
+      // If num === 1 increment curr
+      if (num === 1) curr++;
+      // Else check if curr is > than highest; if so highest = curr
+      else {
+          if (highest < curr) highest = curr;
+          // Then refresh curr
+          curr = 0;
+      }
+  }
+  // Once done looping check one more time if curr is > highest; if so reassign hgihest
+  if (highest < curr) highest = curr;
+  // And return highest
+  return highest;
+}
+
+
+
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
