a5307f01ae8106f826aa121958a4f6cd56b6dc1c
mauiwowie88 <maugarza96@gmail.com>
2023-07-06 18:55:32 -0500

    Jul 6.

--- Arrays/two-sum.js
+++ Arrays/two-sum.js
@@ -12,18 +12,18 @@ pairSum([4, 7, 9, 2, 5, 1], 5); // -> [0, 5]
 pairSum([1, 6, 7, 2], 13); // -> [1, 2]
 */
 
-// 1st attempt:
+// 1st attempt: time O(n) | space O(n)
 const twoSum = (arr, target) => {
     // Cache to store visited numbers
-    const cache = {};
+    const cache = new Map();
     // Loop thru input arr
     for (let i = 0; i < arr.length; i++) {
-        // Store the diff between target and curr num
-        const diff = target - arr[i];
-        // If cache has the diff; we found match return indeces
-        if (cache[diff]) return [cache[diff], i];
-        // Else store curr num and its index
-        else cache[arr[i]] = i;
+      // Store the diff between target and curr num
+      const diff = target - arr[i];
+      // If cache has the diff; we found match return indeces
+      if (cache.has(diff)) return [cache.get(diff), i];
+      // Else store curr num and its index
+      else cache.set(arr[i], i);
     }
     // If you reach this no matches found; return false
     return false;
